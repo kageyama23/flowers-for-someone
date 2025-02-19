937927e57d96ca17bd99110be135a6f6598799d5
kageyama23 <gamerking29king@gmail.com>
2025-02-20 01:59:13 +0800

    name cahnge

--- js/flowers.js
+++ js/flowers.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HELLO LESTER').split('')
+    const titles = ('HELLO DANICA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
